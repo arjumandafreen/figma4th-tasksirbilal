70fd5ddeed4542458c90c5c446470883a14ef92a
arjumandafreen <arjumandafreen407@gmail.com>
2024-10-28 01:45:18 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
-import Featured from "@/Components/Featured";
-import Footer from "@/Components/Footer";
-import Hero from "@/Components/Hero";
-import Navbar from "@/Components/Navbar";
-import Recent from "@/Components/Recent";
-
+import Featured from "@/components/Featured"; // Updated to lowercase 'c'
+import Footer from "@/components/Footer";       // Updated to lowercase 'c'
+import Hero from "@/components/Hero";           // Updated to lowercase 'c'
+import Navbar from "@/components/Navbar";        // Updated to lowercase 'c'
+import Recent from "@/components/Recent";        // Updated to lowercase 'c'
 
 export default function Home() {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <Recent/>
-      <Featured/>
-      <Footer/>
+      <Navbar />
+      <Hero />
+      <Recent />
+      <Featured />
+      <Footer />
     </div>
   );
 }
